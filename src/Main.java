a7f0569149c0a5543e8153ce704db5b2a413f059
And-Rom <you@example.com>
2023-02-27 20:57:47 +0300

    Added lesson6 task1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,36 @@
+import java.util.HashSet;
+import java.util.Set;
+import java.util.concurrent.ThreadLocalRandom;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        /*
+        1. Напишите метод, который заполнит массив из 1000 элементов случайными цифрами от 0 до 24.
+        2. Создайте метод, в который передайте заполненный выше массив и с помощью Set вычислите процент
+        уникальных значений в данном массиве и верните его в виде числа с плавающей запятой.
+        Для вычисления процента используйте формулу: процент уникальных чисел = количество уникальных чисел * 100 / общее количество чисел в массиве.
+        */
+
+        int size = 10;
+        int[] array = new int[size];
+
+        for (int i = 0; i < size; i++) {
+            array[i] = ThreadLocalRandom.current().nextInt(25);
+        }
+        System.out.println(uniqueNumbersStat(array));
+    }
+
+    private static Set uniqueNumbersStat(int[] array) {
+        Set<Integer> set = new HashSet<>();
+        Set<Integer> tempSet = new HashSet<>();
+        for (int i = 0; i < array.length; i++) {
+            if (set.contains(array[i])) {
+                tempSet.add(array[i]);
+            } else {
+                set.add(array[i]);
+            }
+        }
+        set.removeAll(tempSet);
+        return set;
     }
 }
